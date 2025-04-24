8bca0193e1e6069a84c7df330d52662890129b82
Witwin12 <kunwin12@gmail.com>
2025-04-24 17:02:00 +0700

    make sound effect when click

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,41 @@
 import { Text, View, Pressable} from "react-native";
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
+import {Audio} from "expo-av";
 
 export default function Index() {
   const [score, setscore] = useState(0);
+  const [sound, setsound] = useState<Audio.Sound | null>(null);
+
+  useEffect(() => {
+    const loadSound = async () => {
+      const { sound } = await Audio.Sound.createAsync(
+        require("../assets/sound/testsound.mp3")
+      );
+      setsound(sound);
+    };  
+
+    loadSound();
+
+    // clear sound
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, []);
+
+  const handlePress = async () => {
+    setscore(score + 1);
+    if (sound) {
+      await sound.replayAsync(); // replay sound
+    }
+  };
   return (
     // make button and show score
     <View>
       <Text style={{ fontSize: 32, marginBottom: 20 }}>{score}</Text>
   <Pressable
-    onPress={() => setscore (score + 1)}
+    onPress={handlePress}
     style={({ pressed }) => ({
       backgroundColor: pressed ? 'lightgray' : 'skyblue',
       padding: 10,
